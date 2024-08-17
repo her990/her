15a96874f8aadc003d864ac0b3a66047533f7a1f
her990 <walajaun@gmail.com>
2024-08-17 22:39:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function checkPassword() {
     var password = document.getElementById("password").value;
 
     // Replace "your_password" with the actual password you want to use
-    if (password === "just wanna scream") {
+    if (password === "my eternal love") {
         window.location.href = "nextindex.html";
     } else {
-        alert("sorry,aap nhi dekh sakte");
+        alert("chut me mila dunga bsdk chala jaa");
     }
 }
